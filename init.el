06a7f973515364d84f71cd6baa8a1426a6b3dde6
INOUE Takuya <inouetakuya5@gmail.com>
2017-02-20 19:01:54 +0900

    デフォルトの文字コードを utf8 に設定した

--- init.el
+++ init.el
@@ -72,6 +72,10 @@
 ;;         (concat '"/Users/inouetakuya/info:"
 ;;                 (getenv "INFOPATH")))
 
+;; Emacs における日本語文字コードの設定 | プログラマーズ雑記帳
+;; http://yohshiy.blog.fc2.com/blog-entry-273.html
+(prefer-coding-system 'utf-8)
+
 ;; iswitchb.el
 ;; バッファ切替を強化する
 ;; Emacs テクニックバイブル P85
