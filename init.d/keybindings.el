5f78f8bcf7ce1d21ce2f14931a1d062ef35d498d
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-06 23:20:27 +0900

    同じコマンドを連続実行したときの振舞いを変更する

--- init.d/keybindings.el
+++ init.d/keybindings.el
@@ -1,2 +1,8 @@
 ;; キーバインド（グローバル）
 (global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; sequential-command.el
+;; 同じコマンドを連続実行したときの振舞いを変更する
+;; Emacs テクニックバイブル P76
+(require 'sequential-command-config)
+(sequential-command-setup-keys)
--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; Emacs ビギナーに贈る、これからバリバリ使い隊!!人のための設定講座 その1。 - 日々、とんは語る。
+;; http://d.hatena.ne.jp/tomoya/20090121/1232536106
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+
 ;; 初期設定ファイルの読み込み
 (load "~/.emacs.d/init.d/keybindings.el")
 (load "~/.emacs.d/init.d/view.el")
