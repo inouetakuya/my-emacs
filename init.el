181d3840cc4d32532b5246c981831208c87fb898
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-06 23:08:57 +0900

    起動時にロゴを表示させないようにした

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
 ;; 初期設定ファイルの読み込み
 (load "~/.emacs.d/init.d/keybindings.el")
 (load "~/.emacs.d/init.d/view.el")
+
+;; 起動時にロゴを表示させない
+(setq inhibit-startup-message t)
+
+;; ビープ音を消す
+(setq visible-bell t)
